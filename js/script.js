357ae45540dbe893ce4352ca75c524aa1e4a00c0
Carlomadella <madella871@gmail.com>
2025-03-23 22:55:11 +0100

    creata scaletta primo esercizio

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Numero minimo di push: 7
+
+// PRIMO ESERCIZIO: Palindroma
+// 
+// Scaletta:
+// 1. Chiedere all’utente di inserire una parola:
+//  1a. creare una varabile chiamata word che contenga il valore del prompt
+//  1b. creare una variabile chiamata reversedWord che utilizza il metodo reverse per contenere il valore
+// creare un array chiamato wordArray per contenere i caratteri spezzettati del prompt col metodo text.split("")
+// 2. Creare una funzione per capire se la parola inserita è palindroma:
+//  2a. creare una funzione chiamata pal con parametro word
+//  2b. creare un if per controllare se la variabile è uguale alla parola al contrario
+//  2c. stampare il risultato
+//  2d. richiamare la funzione
+
+
